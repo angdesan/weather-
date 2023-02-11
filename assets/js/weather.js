9a5fa6254c3a29bc1581a97a48533ecb2e0d230c
angdesan <angdesan99@gmail.com>
2023-02-11 12:19:36 -0500

    parte 3 finalizada

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,72 +1,72 @@
 import {weather_data} from './data.js';
 
 
-let loadDayForecastData = () => {
+let loadDayForecastData = (indice=0) => {
 	let id = "city";
     let city = document.getElementById(id);
     console.log(city);
-    city.innerHTML = weather_data[0].city;
+    city.innerHTML = weather_data[indice].city;
 
     let idDate = "date";
     let date = document.getElementById(idDate);
-    date.innerHTML = weather_data[0].date;
+    date.innerHTML = weather_data[indice].date;
 
     let idMaxtemperature = 'maxtemperature';
     let maxtemperature = document.getElementById(idMaxtemperature);
-    maxtemperature.innerHTML = weather_data[0].maxtemperature;
+    maxtemperature.innerHTML = weather_data[indice].maxtemperature;
 
     let idMintemperature = 'mintemperature';
     let mintemperature = document.getElementById(idMintemperature);
-    mintemperature.innerHTML = weather_data[0].mintemperature;
+    mintemperature.innerHTML = weather_data[indice].mintemperature;
 
     let idCloudiness = 'cloudiness';
     let cloudiness = document.getElementById(idCloudiness);
-    cloudiness.innerHTML = weather_data[0].cloudiness;
+    cloudiness.innerHTML = weather_data[indice].cloudiness;
 
     let idWind = 'wind';
     let wind = document.getElementById(idWind);
-    wind.innerHTML = weather_data[0].wind;
+    wind.innerHTML = weather_data[indice].wind;
 
     let idRainfall = 'rainfall';
     let rainfall = document.getElementById(idRainfall);
-    rainfall.innerHTML = weather_data[0].rainfall;
+    rainfall.innerHTML = weather_data[indice].rainfall;
 
 
 
 
     let idLate_icon = 'late_icon';
     let late_icon = document.getElementById(idLate_icon);
-    late_icon.innerHTML = weather_data[0].forecast_today[0].icon
+    late_icon.innerHTML = weather_data[indice].forecast_today[0].icon
 
     let idLate_temperature = 'late_temperature';
     let late_temperature= document.getElementById(idLate_temperature);
-    late_temperature.innerHTML = weather_data[0].forecast_today[0].temperature;
+    late_temperature.innerHTML = weather_data[indice].forecast_today[0].temperature;
 
     let idLate_forecast = 'late_forecast';
     let late_forecast = document.getElementById(idLate_forecast);
-    late_forecast.innerHTML =  weather_data[0].forecast_today[0].forecast;
+    late_forecast.innerHTML =  weather_data[indice].forecast_today[0].forecast;
 
     let idLate_text = 'late_text';
     let late_text = document.getElementById(idLate_text);
-    late_text.innerHTML = weather_data[0].forecast_today[0].text;
+    late_text.innerHTML = weather_data[indice].forecast_today[0].text;
 
 
 
     let idNight_icon = 'night_icon';
     let night_icon = document.getElementById(idNight_icon);
-    night_icon.innerHTML = weather_data[0].forecast_today[1].icon
+    night_icon.innerHTML = weather_data[indice].forecast_today[1].icon
 
     let idNight_temperature = 'night_temperature';
     let night_temperature= document.getElementById(idNight_temperature);
-    night_temperature.innerHTML = weather_data[0].forecast_today[1].temperature;
+    night_temperature.innerHTML = weather_data[indice].forecast_today[1].temperature;
 
     let idNight_forecast = 'night_forecast';
     let night_forecast = document.getElementById(idNight_forecast);
-    night_forecast.innerHTML =  weather_data[0].forecast_today[1].forecast;
+    night_forecast.innerHTML =  weather_data[indice].forecast_today[1].forecast;
 
     let idNight_text = 'night_text';
     let night_text = document.getElementById(idNight_text);
-    night_text.innerHTML = weather_data[0].forecast_today[1].text;
+    night_text.innerHTML = weather_data[indice].forecast_today[1].text;
 
     
 
@@ -102,14 +102,49 @@ let loadWeekForecastData = () => {
 	
 }
 
-document.addEventListener("DOMContentLoaded", (event) => {
+/* document.addEventListener("DOMContentLoaded", (event) => {
     //Código a ejecutar
     loadDayForecastData();
-});
+}); */
   
+let selectList = () =>{
+    let element = document.getElementById("dropdownMenuButton");
+    let message = `<option value="" selected disabled hidden>Seleccione una ciudad</option>
+    <option class="dropdown-item" value="guayaquil">Guayaquil</option>
+    <option class="dropdown-item" value="ambato">Ambato</option>
+    <option class="dropdown-item" value="tena">Tena</option>`
+
+    element.innerHTML = message;
+}
+selectList();
+
+  
+let elemento = document.getElementById("dropdownMenuButton");
+elemento.addEventListener('change', (event) => {
+    //Código a ejecutar
+    //El event contiene la información del elemento seleccionado
+    let selectedValue = event.target.value 
+    if (selectedValue === "guayaquil"){
+        loadDayForecastData(0);
+
+
+    }else if (selectedValue ==="ambato"){
+        loadDayForecastData(1);
+
+    }else if (selectedValue === "tena"){
+        loadDayForecastData(2);
+
+    }
+});
+
+
 let element = document.getElementById("loadinfo");
 
 element.addEventListener('click', (event) => {
     //Código a ejecutar
     loadWeekForecastData();
+    
 });
+
+
+
